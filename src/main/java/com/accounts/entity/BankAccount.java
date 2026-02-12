536738d88fe333eb1609e71774e867e98d1e254f
admin <rong@agile-solutions.ch>
2026-02-12 17:32:43 +0100

    global exceptoin handler

--- src/main/java/com/accounts/entity/BankAccount.java
+++ src/main/java/com/accounts/entity/BankAccount.java
@@ -1,10 +1,13 @@
 package com.accounts.entity;
 
+import com.accounts.domain.Client;
 import com.accounts.domain.Currency;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 
 @NoArgsConstructor
@@ -24,8 +27,17 @@ public class BankAccount {
     private Currency currency;
 
     @Column
+    private String country;
+
+    @Column
     private Float balance;
 
     @Column
     private String status;
+
+    @Column
+    private Float transferLimit;
+
+    @Column
+    private LocalDateTime accountCreateDate;
 }
